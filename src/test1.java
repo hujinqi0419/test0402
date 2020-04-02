89164fbe1926a47aee1eb364824bdb20d375424e
hujinqi0419 <62985098+hujinqi0419@users.noreply.github.com>
2020-04-02 09:39:25 +0900

    Update test1.java

--- src/test1.java
+++ src/test1.java
@@ -2,4 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("第一次上传");
     }
+     public static void main(String[] args) {
+        System.out.println("第er次下载");
+    }
 }
